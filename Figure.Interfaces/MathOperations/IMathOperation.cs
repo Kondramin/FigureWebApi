1a4358e55806978673641c4cd02b62f8ee1d8266
Артём Кондратьев <exitway12@gmail.com>
2022-02-10 16:03:35 +0300

    Dev: Realized MathOperations service

---
+++ Figure.Interfaces/MathOperations/IMathOperation.cs
@@ -0,0 +1,9 @@
+﻿using Figure.Data.Entity.Entities.Base;
+
+namespace Figure.Interfaces.MathOperations
+{
+    public interface IMathOperation
+    {
+        double CalculateArea(FigureEntity figureEntity);
+    }
+}
---
+++ Figure.Services/MathOperatoins/MathOperation.cs
@@ -0,0 +1,37 @@
+﻿using Figure.Data.Entity.Entities;
+using Figure.Data.Entity.Entities.Base;
+using Figure.Interfaces.MathOperations;
+using System;
+
+namespace Figure.Services.MathOperatoins
+{
+    public class MathOperation : IMathOperation
+    {
+        public double CalculateArea(FigureEntity figureEntity)
+        {
+
+            if (figureEntity.CountOfAngles == 0)
+            {
+                if (!(figureEntity is Circle circle)) throw new ArgumentException(nameof(figureEntity));
+                return CalculateCircleArea(circle);
+            }
+            
+            if (!(figureEntity is Triangle triangle)) throw new ArgumentException(nameof(figureEntity));
+
+            return CalculateTriangleArea(triangle);
+        }
+
+
+        private double CalculateCircleArea(Circle circle)
+        {
+            return Math.PI * Math.Pow(circle.Radius, 2);
+        }
+
+        private double CalculateTriangleArea(Triangle triangle)
+        {
+            var hafPerimetr = (triangle.SideA + triangle.SideB + triangle.SideC) / 2;
+
+            return Math.Sqrt(hafPerimetr * (hafPerimetr - triangle.SideA) * (hafPerimetr - triangle.SideB) * (hafPerimetr - triangle.SideC));
+        }
+    }
+}
