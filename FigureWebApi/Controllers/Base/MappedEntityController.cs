09c9c0b4969daa68cb74c25f287092ae600ca370
Артём Кондратьев <exitway12@gmail.com>
2022-02-11 14:23:48 +0300

    Dev: Realized generic abstract controller, circle and triangle controllers

--- FigureWebApi/Controllers/Base/MappedEntityController.cs
+++ FigureWebApi/Controllers/Base/MappedEntityController.cs
@@ -1,14 +1,67 @@
-﻿using Figure.Interfaces.Base.Entities;
+﻿using AutoMapper;
+using Figure.Data.Entity.Entities.Base;
+using Figure.Interfaces.Base.Entities;
+using Figure.Interfaces.Base.Repositories;
+using Figure.Interfaces.MathOperations;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace FigureWebApi.Controllers.Base
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class MappedEntityController<T, TBase>
-        where T : IEntity, new()
-        where TBase : IMappedEntity, new()
+    public abstract class MappedEntityController<T, TBase> : ControllerBase
+        where T : IMappedEntity, new()
+        where TBase : IEntity, new()
     {
-        //TODO: Realize controller
+        private readonly IRepository<TBase> _Repository;
+        private readonly IMapper _Mapper;
+        private readonly IMathOperation _MathOperation;
+
+        public MappedEntityController(IRepository<TBase> repository, IMapper mapper, IMathOperation mathOperation)
+        {
+            _Repository = repository;
+            _Mapper = mapper;
+            _MathOperation = mathOperation;
+        }
+
+
+        protected virtual T GetT(TBase item) => _Mapper.Map<T>(item);
+        protected virtual IEnumerable<T> GetT(IEnumerable<TBase> items) => _Mapper.Map<IEnumerable<T>>(items);
+
+        protected virtual TBase GetBase(T item) => _Mapper.Map<TBase>(item);
+        protected virtual IEnumerable<TBase> GetBase(IEnumerable<T> items) => _Mapper.Map<IEnumerable<TBase>>(items);
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> AddNew(T item)
+        {
+            if (item is null) return BadRequest();
+            await _Repository.AddAsync(GetBase(item));
+
+            return Ok(item);
+        }
+
+
+        [HttpGet("id/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(double))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(int))]
+        public async Task<IActionResult> GetById(int id)
+        {
+            if (id == 0) return BadRequest();
+            var item = await _Repository.GetAsync(id);
+            if (item is null) return NotFound(id);
+
+            if (item is not FigureEntity figureEntity) return BadRequest();
+            
+            var result = _MathOperation.CalculateArea(figureEntity);
+
+            return Ok(result);
+        }
     }
 }
---
+++ FigureWebApi/Controllers/CircleController.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Figure.Data.Entity.Entities;
+using Figure.Interfaces.Base.Repositories;
+using Figure.Interfaces.MathOperations;
+using FigureWebApi.Controllers.Base;
+using FigureWebApi.DtoModels;
+
+namespace FigureWebApi.Controllers
+{
+    public class CircleController : MappedEntityController<CircleDto, Circle>
+    {
+        public CircleController(IRepository<Circle> repository, IMapper mapper, IMathOperation mathOperation) : base(repository, mapper, mathOperation) { }
+    }
+}
